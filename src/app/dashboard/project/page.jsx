7e76987306a2ef0c6ec06e5b49dc05e678b6f717
Rithick <rithickjackson8@gmail.com>
2025-09-08 04:08:19 +0530

    updated some links

--- src/app/dashboard/project/page.jsx
+++ src/app/dashboard/project/page.jsx
@@ -50,7 +50,7 @@ function Projects() {
 
       ],
       image: "/Resume.png",
-      github: "https://ai-resume-builder-rj.netlify.app/",
+      github: "https://github.com/Rithick78/ai-resume-builder",
       live: "https://ai-resume-builder-rj.netlify.app/",
       category: "Development"
     },
